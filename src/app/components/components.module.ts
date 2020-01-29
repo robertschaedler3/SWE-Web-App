8f30e6a580329580fa0c2fd48e51bfc6a597047b
Robert Schaedler III <robertschaedler3@gmail.com>
2020-01-29 15:50:29 -0500

    update components.module.ts

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,61 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatTooltipModule,
+  MatToolbarModule,
+  MatSelectModule,
+  MatProgressBarModule,
+  MatDividerModule,
+  MatListModule,
+  MatSlideToggleModule,
+  MatExpansionModule,
+  MatCardModule,
+  MatIconModule,
+  MatDialogModule,
+  MatSliderModule
+} from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSliderModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSliderModule
   ]
 })
-export class ComponentsModule { }
+export class ComponentModule { }
