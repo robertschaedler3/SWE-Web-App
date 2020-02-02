5331b84b71c81a8c541ae79c3b252188b3a0a72d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-02 17:00:16 -0500

    configure main-layout routing

--- src/app/layouts/main-layout/main-layout.module.ts
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainLayoutComponent } from './main-layout.component';
+
+import { MainLayoutRoutingModule } from './main-layout-routing.module';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [MainLayoutComponent],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MainLayoutRoutingModule
   ]
 })
 export class MainLayoutModule { }
