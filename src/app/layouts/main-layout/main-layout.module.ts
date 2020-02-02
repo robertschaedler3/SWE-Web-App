213dfe0dcc20c21355d0a9a091b6807fcb6226fb
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-02 16:41:10 -0500

    create main-layout

---
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './main-layout.component';
+
+
+
+@NgModule({
+  declarations: [MainLayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainLayoutModule { }
