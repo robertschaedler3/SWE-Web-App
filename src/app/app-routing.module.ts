1c1b84a4d7de7b8a3c0234b34a6b88cbbc4651f9
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-02 16:59:47 -0500

    add routing for main layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: './layouts/main-layout/main-layout.module#MainLayoutModule'
+    }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
