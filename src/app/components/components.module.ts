d882804e2d7f355a0c4ea4c6d1eb8307a1bc9f85
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-09 20:48:09 -0500

    change shared directory to utils

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaterialModule } from '../shared/material.module';
+import { MaterialModule } from '../utils/material.module';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
--- src/app/layouts/main-layout/main-layout.module.ts
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaterialModule } from '../../shared/material.module';
+import { MaterialModule } from '../../utils/material.module';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
