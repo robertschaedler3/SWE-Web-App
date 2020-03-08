b609417176d6b7196ffbafe843648b8d2625f6ba
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-08 01:17:40 -0500

    restore material imports to main layout

--- src/app/layouts/main-layout/main-layout.module.ts
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../utils/material.module';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
@@ -15,6 +16,9 @@ import { CreateEventDialogComponent } from 'src/app/create-event-dialog/create-e
   imports: [
     CommonModule,
     MainLayoutRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   entryComponents: [CreateEventDialogComponent]
 })
