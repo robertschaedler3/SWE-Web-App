18a56d319328dcf14962671d0e75d5f0e7773b72
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-16 17:01:50 -0400

    update auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user.model';
+import { switchMap } from 'rxjs/operators';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  user$: Observable<User>
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>(`user/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  async googleSignin() {
+    const provider = new auth.GoogleAuthProvider();
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
+  }
+
+  async signOut() {
+    this.afAuth.auth.signOut();
+    return this.router.navigate(['/login']);
+  }
+
+  private updateUserData({ uid, displayName, email, photoURL }: User) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`user/${uid}`);
+    const data = {
+      uid,
+      displayName,
+      email,
+      photoURL
+    };
+
+    return userRef.set(data, { merge: true });
+  }
+
 }
