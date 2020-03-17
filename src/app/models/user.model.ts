65a1d8f7438c19796acdd53555f1047295c433c9
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-16 22:30:26 -0400

    update user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,11 +1,13 @@
-import { Tag } from './tag.model';
-import { StevensEvent } from './event.model';
+export interface Role {
+    student?: boolean;
+    organizer?: boolean;
+    admin?: boolean;
+}
 
 export interface User {
     uid: string;
     displayName: string;
     email: string;
     photoURL: string;
-    attending?: StevensEvent[];
-    subscribed?: Tag[];
+    roles: Role;
 }
