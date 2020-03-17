3f3751a43bc0398dbde24e8c98fdab23132d7fe0
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-16 22:22:50 -0400

    fix auth guard routing

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { AuthService } from './auth.service';
+import { AuthService } from '../services/auth.service';
+import { take, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,17 @@ export class AuthGuard implements CanActivate, CanActivateChild {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log('router guard');
-    // if (this.auth.authenticated) return true;
-    // this.router.navigate(['/login']);
-    return true;
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => user ? true : false),
+      tap(loggedIn => {
+        if (!loggedIn) {
+          console.log('Access denied. User is not logged in')
+          this.router.navigate(['/login']);
+        }
+      }),
+    )
+
   }
   canActivateChild(
     next: ActivatedRouteSnapshot,
