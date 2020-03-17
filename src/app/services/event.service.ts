8da4e153e8c7342a9f31a87dda32f64a922b8662
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-16 23:17:48 -0400

    update event service

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { StevensEvent } from '../models/event.model';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 
 @Injectable({
@@ -11,20 +11,16 @@ import { StevensEvent } from '../models/event.model';
 })
 export class EventService {
 
-  public events$: Observable<StevensEvent[]>;
+  public upcoming$: Observable<StevensEvent[]>;
 
-  constructor(private http: HttpClient) { }
-
-  public getEvents() {
-    // TODO: Add pagination/dynamic query on scroll
-    return this.events$ = this.http.get<StevensEvent>(`/api/events`).pipe(
-      map(data => _.values(data)),
-      tap(console.log),
-    );
+  constructor(
+    private afs: AngularFirestore,
+  ) {
+    this.upcoming$ = this.afs.collection<StevensEvent>('/event').valueChanges();
   }
 
   public createEvent(data: StevensEvent) {
-    return this.http.post<StevensEvent>(`/api/events`, data);
+    this.afs.collection<StevensEvent>('/event').add(data);
   }
 
 }
