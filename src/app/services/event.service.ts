4a0cc6655a4a1e868af116caa7794f23b9e9857c
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-20 09:59:42 -0400

    update event creation to add tags

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { StevensEvent } from '../models/event.model';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { DialogResult } from '../create-event-dialog/create-event-dialog.component';
 
+interface EventTag {
+  tagId: string;
+  eventId: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +20,23 @@ export class EventService {
   constructor(
     private afs: AngularFirestore,
   ) {
-    // this.upcoming$ = this.afs.collection<StevensEvent>('/event').valueChanges();
     let now = new Date();
-    let future = new Date(now.getTime() + 604800000);
-    this.upcoming$ = this.afs.collection<StevensEvent>('/event', ref => ref.orderBy('start').startAt(now).endAt(future)).valueChanges();
+    let future = new Date(now.getTime() + 6048000000);
+    this.upcoming$ = this.afs.collection<StevensEvent>('/event', ref => ref.orderBy('start').startAt(now).endAt(future)).valueChanges({ idField: 'id' });
   }
 
-  public createEvent(data: StevensEvent) {
-    this.afs.collection<StevensEvent>('/event').add(data);
+  public createEvent(data: DialogResult) {
+    console.log(data);
+    let newEvent: StevensEvent = data.event;
+    this.afs.collection<StevensEvent>('/event').add(newEvent).then(event => {
+      data.tags.forEach(tag => {
+        this.afs.collection<EventTag>('/event_tags').doc(`${tag.id}_${event.id}`).set({
+          tagId: tag.id,
+          eventId: event.id
+        }, { merge: true });
+      });
+
+    });
   }
 
 }
