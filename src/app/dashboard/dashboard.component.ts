b698301e547b4c1a32b7b87dfd654417378268db
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-20 09:55:45 -0400

    add infection for event details dialog

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../services/event.service';
-import { CreateEventDialogComponent } from '../create-event-dialog/create-event-dialog.component';
+import { CreateEventDialogComponent, DialogResult } from '../create-event-dialog/create-event-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
 import { StevensEvent } from '../models/event.model';
 import { AuthService } from '../services/auth.service';
 import { User } from '../models/user.model';
+import { EventDetailsDialogComponent } from '../event-details-dialog/event-details-dialog.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -29,8 +30,15 @@ export class DashboardComponent implements OnInit {
       data: { author: user.displayName, uid: user.uid }
     });
 
-    dialogRef.afterClosed().subscribe((result: StevensEvent) => {
+    dialogRef.afterClosed().subscribe((result: DialogResult) => {
       if (result) this.events.createEvent(result);
+    });
+  }
+
+  public displayDetails(data) {
+    this.dialog.open(EventDetailsDialogComponent, {
+      width: '800px',
+      data: data
     });
   }
 
