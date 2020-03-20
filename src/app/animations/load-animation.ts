04980a31f35751dca06a1ff72214169a8165e6ad
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-20 19:44:01 -0400

    create animations

---
+++ src/app/animations/load-animation.ts
@@ -0,0 +1,60 @@
+import {
+    transition,
+    trigger,
+    state,
+    style,
+    animate,
+    query,
+    stagger
+} from '@angular/animations';
+
+export const fader =
+    trigger('fader', [
+        state('show', style({
+            opacity: 1
+        })),
+        state('hide', style({
+            opacity: 0
+        })),
+        transition(':enter', [
+            style({
+                opacity: 0,
+            }),
+            animate('600ms 100ms ease-out')
+        ])
+    ]);
+
+export const fadeUp =
+    trigger('fadeUp', [
+        state('show', style({
+            opacity: 1
+        })),
+        state('hide', style({
+            opacity: 0
+        })),
+        transition(':enter', [
+            style({
+                opacity: 0,
+                transform: 'translateY(50px)'
+            }),
+            animate('400ms 100ms ease-out')
+        ])
+    ]);
+
+export const cardListFader =
+    trigger('cardListFader', [
+        transition('* => *', [
+            query('mat-card', style({
+                transform: 'translateY(75px)',
+                opacity: 0
+            })),
+            query('mat-card',
+                stagger('100ms', [
+                    animate('500ms 100ms ease-out', style({
+                        transform: 'translateY(0px)',
+                        opacity: 1
+                    }))
+                ])
+            )
+        ])
+    ]); 
