ee60bcdc89e4711dda4aaff042d270aaceed8286
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 16:02:25 -0400

    display routes

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  { path: '/feed', title: 'Feed', icon: 'dashboard' },
+  { path: '/discover', title: 'Discover', icon: 'search' },
+  { path: '/people', title: 'People', icon: 'people' },
+  { path: '/groups', title: 'Groups', icon: 'group_work' },
+  { path: '/settings', title: 'Settings', icon: 'settings' },
+];
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
+
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  menuItems: any[];
+
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
 
 }
