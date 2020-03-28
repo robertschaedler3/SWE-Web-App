fee76b7d20768895055344d5a9258fa11fa4c00d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-27 23:31:45 -0400

    move search to individual component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { SearchService } from 'src/app/services/search.service';
+import { StevensEvent } from 'src/app/models/event.model';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  results: StevensEvent[];
+  lastKeyPress: number = 0;
+
+  @ViewChild('searchInput') searchInput: ElementRef<HTMLInputElement>;
+
+  constructor(
+    public searchSvc: SearchService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  search($event): void {
+    if ($event.timeStamp - this.lastKeyPress > 300) {
+      let query = $event.target.value;
+      this.searchSvc.getEvents(query, query + '\uf8ff').subscribe(results => {
+        this.results = results;
+      });
+    }
+    this.lastKeyPress = $event.timeStamp;
+  }
+
+  navigateTo(value: MatAutocompleteSelectedEvent) {
+    this.router.navigate(['/details', value.option.value])
+    this.searchInput.nativeElement.value = '';
   }
 
 }
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,9 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { SearchService } from 'src/app/services/search.service';
-import { StevensEvent } from 'src/app/models/event.model';
-import { MatAutocompleteSelectedEvent } from '@angular/material';
-import { Router } from '@angular/router';
 import { fader } from 'src/app/animations/load-animation';
 
 @Component({
@@ -14,33 +10,11 @@ import { fader } from 'src/app/animations/load-animation';
 })
 export class ToolbarComponent implements OnInit {
 
-  results: StevensEvent[];
-  lastKeyPress: number = 0;
-
-  @ViewChild('searchInput') searchInput: ElementRef<HTMLInputElement>;
-
   constructor(
     public auth: AuthService,
-    public searchSvc: SearchService,
-    private router: Router
   ) { }
 
-  ngOnInit() {
-  }
-
-  search($event): void {
-    if ($event.timeStamp - this.lastKeyPress > 300) {
-      let query = $event.target.value;
-      this.searchSvc.getEvents(query, query + '\uf8ff').subscribe(results => {
-        this.results = results;
-      });
-    }
-    this.lastKeyPress = $event.timeStamp;
-  }
-
-  navigateTo(value: MatAutocompleteSelectedEvent) {
-    this.router.navigate(['/details', value.option.value])
-    this.searchInput.nativeElement.value = '';
+  ngOnInit(): void {
   }
 
 }
