4786c6f64244362e19210ab23bf506aad1b934ad
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 15:28:50 -0400

    create people component

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
+import { PeopleComponent } from './people/people.component';
 
 
 
 @NgModule({
   declarations: [
-    FeedComponent
+    FeedComponent,
+    PeopleComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    FeedComponent
+    FeedComponent,
+    PeopleComponent
   ]
 })
 export class ContentModule { }
