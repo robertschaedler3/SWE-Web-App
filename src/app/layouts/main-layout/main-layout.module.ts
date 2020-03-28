519a0a4eac278fb8f04ced78f35280404ca3d88d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 15:33:21 -0400

    update main-layout module

--- src/app/layouts/main-layout/main-layout.module.ts
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -2,33 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ComponentModule } from 'src/app/components/components.module';
+import { ContentModule } from 'src/app/content/content.module';
 import { MaterialModule } from '../../utils/material.module';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
-import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
-import { CreateEventDialogComponent } from 'src/app/create-event-dialog/create-event-dialog.component';
-import { EventDetailsDialogComponent } from '../../event-details-dialog/event-details-dialog.component';
-import { EventDetailsComponent } from '../../event-details/event-details.component';
-import { ComponentModule } from 'src/app/components/components.module';
-import { FeedComponent } from '../../content/feed/feed.component';
-import { ContentModule } from 'src/app/content/content.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    CreateEventDialogComponent,
-    EventDetailsDialogComponent,
-    EventDetailsComponent,
-  ],
+  declarations: [],
   imports: [
     CommonModule,
-    MainLayoutRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule,
     ComponentModule,
-    ContentModule
-  ],
-  entryComponents: [CreateEventDialogComponent]
+    ContentModule,
+    MaterialModule,
+    MainLayoutRoutingModule
+  ]
 })
 export class MainLayoutModule { }
