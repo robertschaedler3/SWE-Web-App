65baaeb20f337dff586ac156cc611236d652a074
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 15:29:51 -0400

    create discover component

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { PeopleComponent } from './people/people.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 
 
 @NgModule({
   declarations: [
     FeedComponent,
-    PeopleComponent
+    PeopleComponent,
+    DiscoverComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     FeedComponent,
-    PeopleComponent
+    PeopleComponent,
+    DiscoverComponent
   ]
 })
 export class ContentModule { }
