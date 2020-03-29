434de7b3b9bfbba8889b01d7b585e98024363f17
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 22:29:53 -0400

    move dashboard layout to feed

--- src/app/content/feed/feed.component.ts
+++ src/app/content/feed/feed.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/services/event.service';
+import { Router } from '@angular/router';
+import { cardListFader } from 'src/app/animations/load-animation';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
+  animations: [cardListFader]
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public events: EventService,
+    private router: Router
+  ) {
+    this.events.getEvents();
+  }
 
   ngOnInit(): void {
   }
 
+  displayDetails(id: string) {
+    console.log(id);
+    this.router.navigate(['/details', id]);
+  }
 }
