d10e761137b37a54153b41dab84e9a2db8a19dd9
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-28 23:48:01 -0400

    add people list

--- src/app/content/people/people.component.ts
+++ src/app/content/people/people.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PeopleService } from 'src/app/services/people.service';
 
 @Component({
   selector: 'app-people',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() { }
+  constructor(public people: PeopleService) { }
 
   ngOnInit(): void {
   }
