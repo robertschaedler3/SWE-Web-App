5d52811f3be55398b18117e3b2296dd51f1ab9ec
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-29 00:25:11 -0400

    create query

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
 
-  constructor() { }
+  people$: Observable<User[]>
+
+  constructor(
+    private afs: AngularFirestore
+  ) {
+    this.people$ = this.afs.collection<User>('user').valueChanges();
+  }
+
 }
