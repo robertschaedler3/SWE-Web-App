71d7bec2cefb62a5689f65c92c40cfea35fe3ff6
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-13 10:07:29 -0400

    update components module
    Co-authored-by: Liam Brew <lbrew@stevens.edu>

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,24 +2,38 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CreateComponent, CreateComponentDialog } from '../content/create/create.component';
+import { BackComponent } from './back/back.component';
+import { SearchComponent } from './search/search.component';
+
 import { MaterialModule } from '../material/material.module';
 
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
-
 @NgModule({
-  declarations: [
-    FooterComponent,
-    NavComponent
-  ],
   imports: [
     CommonModule,
-    MaterialModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
+  ],
+  declarations: [
+    BackComponent,
+    SearchComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CreateComponent,
+    CreateComponentDialog,
   ],
   exports: [
+    BackComponent,
     FooterComponent,
-    NavComponent
-  ]
+    SearchComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CreateComponent,
+  ],
+  entryComponents: [CreateComponentDialog]
 })
-export class ComponentModule { }
+export class ComponentsModule { }
