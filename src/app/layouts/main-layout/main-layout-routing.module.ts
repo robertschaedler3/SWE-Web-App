d7f7e75350d7c378fa3938131aadb636dca0ab63
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-13 09:51:25 -0400

    update import paths and routing

--- src/app/layouts/main-layout/main-layout-routing.module.ts
+++ src/app/layouts/main-layout/main-layout-routing.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FeedDetailsComponent } from 'src/app/content/feed-details/feed-details.component';
+import { FeedComponent } from '../../content/feed/feed.component';
+import { FeedDetailsComponent } from '../../content/feed-details/feed-details.component';
+import { PeopleComponent } from '../../content/people/people.component';
+import { DiscoverComponent } from '../../content/discover/discover.component';
+import { SettingsComponent } from '../../content/settings/settings.component';
 
-import { FeedComponent } from 'src/app/content/feed/feed.component';
-import { PeopleComponent } from 'src/app/content/people/people.component';
-import { GroupsComponent } from 'src/app/content/groups/groups.component';
-import { SettingsComponent } from 'src/app/content/settings/settings.component';
-import { DiscoverComponent } from 'src/app/content/discover/discover.component';
-
-
-
-const routes: Routes = [
+export const routes: Routes = [
     { path: 'feed', component: FeedComponent },
+    { path: 'details/:eventId', component: FeedDetailsComponent },
     { path: 'discover', component: DiscoverComponent },
     { path: 'people', component: PeopleComponent },
-    { path: 'groups', component: GroupsComponent },
     { path: 'settings', component: SettingsComponent },
-    { path: 'details', redirectTo: 'feed' },
-    { path: 'details/:eventId', component: FeedDetailsComponent },
 ];
 
 @NgModule({
