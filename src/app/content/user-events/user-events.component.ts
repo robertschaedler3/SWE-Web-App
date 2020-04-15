4507814f9b1786a44929d08fd31fe138212ebd05
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-15 11:34:26 -0400

    add user created events only

--- src/app/content/user-events/user-events.component.ts
+++ src/app/content/user-events/user-events.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StevensEvent } from '../../models/event.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-events',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEventsComponent implements OnInit {
 
-  constructor() { }
+  events$: Observable<StevensEvent[]>;
+
+  constructor(
+    private afs: AngularFirestore,
+    private afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
+    this.afAuth.authState.pipe(
+      map(user => {
+        return this.afs.collection<StevensEvent>('/event', ref => ref.where('authorId', '==', user.uid)).valueChanges();
+      })
+    ).subscribe(events => this.events$ = events)
   }
 
 }
