ff424949e4e025df33c320146ff516d7eb4cf53f
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-16 22:03:48 -0400

    add my-events route

--- src/app/layouts/main-layout/main-layout-routing.module.ts
+++ src/app/layouts/main-layout/main-layout-routing.module.ts
@@ -6,12 +6,20 @@ import { FeedDetailsComponent } from '../../content/feed-details/feed-details.co
 import { PeopleComponent } from '../../content/people/people.component';
 import { DiscoverComponent } from '../../content/discover/discover.component';
 import { SettingsComponent } from '../../content/settings/settings.component';
+import { UserEventsComponent } from '../../content/user-events/user-events.component';
+import { UserEventsEditComponent } from '../../content/user-events-edit/user-events-edit.component';
 
 export const routes: Routes = [
     { path: 'feed', component: FeedComponent },
-    { path: 'details/:eventId', component: FeedDetailsComponent },
+    { path: 'details/:id', component: FeedDetailsComponent },
     { path: 'discover', component: DiscoverComponent },
     { path: 'people', component: PeopleComponent },
+    {
+        path: 'my-events', component: UserEventsComponent,
+        children: [
+            { path: ':id', component: UserEventsEditComponent }
+        ]
+    },
     { path: 'settings', component: SettingsComponent },
 ];
 
