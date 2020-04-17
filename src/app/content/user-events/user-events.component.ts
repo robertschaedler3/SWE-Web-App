70e089db8b576581867366e8d4ae7055a30c3da0
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-16 21:57:54 -0400

    dipslay events and dynamically show side panel

--- src/app/content/user-events/user-events.component.ts
+++ src/app/content/user-events/user-events.component.ts
@@ -4,15 +4,19 @@ import { StevensEvent } from '../../models/event.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { map } from 'rxjs/operators';
+import { listFader } from '../../animations/load-animation';
 
 @Component({
   selector: 'app-user-events',
   templateUrl: './user-events.component.html',
-  styleUrls: ['./user-events.component.scss']
+  styleUrls: ['./user-events.component.scss'],
+  animations: [listFader]
 })
 export class UserEventsComponent implements OnInit {
 
   events$: Observable<StevensEvent[]>;
+
+  showOutlet: boolean;
 
   constructor(
     private afs: AngularFirestore,
@@ -22,9 +26,17 @@ export class UserEventsComponent implements OnInit {
   ngOnInit(): void {
     this.afAuth.authState.pipe(
       map(user => {
-        return this.afs.collection<StevensEvent>('/event', ref => ref.where('authorId', '==', user.uid)).valueChanges();
+        return this.afs.collection<StevensEvent>('/event', ref => ref.where('authorId', '==', user.uid)).valueChanges({ idField: 'id' });
       })
-    ).subscribe(events => this.events$ = events)
+    ).subscribe(events => this.events$ = events);
+  }
+
+  onActivate(event: any) {
+    this.showOutlet = true;
+  }
+
+  onDeactivate(event: any) {
+    this.showOutlet = false;
   }
 
 }
