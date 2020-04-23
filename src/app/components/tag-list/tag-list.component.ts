89509d70bcb6bcf70800e2760866c1a373e5f6fc
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-23 12:50:37 -0400

    add tag list input

--- src/app/components/tag-list/tag-list.component.ts
+++ src/app/components/tag-list/tag-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Tag } from '../../models/tag.model';
+import { chipListFader } from '../../animations/load-animation';
 
 @Component({
   selector: 'app-tag-list',
   templateUrl: './tag-list.component.html',
-  styleUrls: ['./tag-list.component.scss']
+  styleUrls: ['./tag-list.component.scss'],
+  animations: [chipListFader]
 })
-export class TagListComponent implements OnInit {
+export class TagListComponent {
+
+  @Input() tags: Tag[];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
