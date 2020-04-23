b79a73283ffffef4370413f8160d2c3fe3df5e14
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-23 12:56:43 -0400

    update event service

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import * as _ from 'lodash';
+import { switchMap, map } from 'rxjs/operators';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase';
+
 import { StevensEvent } from '../models/event.model';
 import { EventTag } from '../models/tag.model';
-import { TagChip } from '../content/create/create.component';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { switchMap } from 'rxjs/operators';
-import { firestore } from 'firebase';
+import { Tag } from '../models/tag.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,15 +20,30 @@ export class EventService {
     private afs: AngularFirestore,
   ) { }
 
-  public createEvent(event: StevensEvent, tags: TagChip[]) {
+
+  private addTags(eventId: string, tags: Tag[]) {
+    for (let i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      this.afs.collection<EventTag>('/event_tags').doc(`${tag.id}_${eventId}`).set({
+        tagId: tag.id,
+        eventId: eventId
+      }, { merge: true });
+    }
+  }
+
+  private deleteTags(eventId: string) {
+    this.afs.collection<EventTag>('event_tag', ref => ref.where('eventId', '==', eventId)).get().pipe(
+      map(tags => {
+        tags.forEach(tag => {
+          this.afs.doc(`event_tag/${tag.id}`).delete();
+        })
+      })
+    )
+  }
+
+  public createEvent(event: StevensEvent, tags: Tag[]) {
     this.afs.collection<StevensEvent>('/event').add(event).then(event => {
-      for (let i = 0; i < tags.length; i++) {
-        var tag = tags[i];
-        this.afs.collection<EventTag>('/event_tags').doc(`${tag.id}_${event.id}`).set({
-          tagId: tag.id,
-          eventId: event.id
-        }, { merge: true });
-      }
+      this.addTags(event.id, tags);
     });
   }
 
@@ -41,21 +57,26 @@ export class EventService {
     return this.afs.doc<StevensEvent>(`/event/${id}`).valueChanges();
   }
 
-  public updateEvent(id: string, data: StevensEvent): void {
-
+  public updateEvent(id: string, data: StevensEvent, tags: Tag[]): void {
+    this.afs.doc<StevensEvent>(`event/${id}`).update(data).then(() => {
+      this.addTags(id, tags);
+    });
   }
 
-  public deleteEvent(id: string): void {
-
+  public deleteEvent(eventId: string): void {
+    this.afs.doc(`event/${eventId}`).delete().then(() => {
+      this.deleteTags(eventId);
+    })
   }
 
-  public getTags(eventId: string): Observable<EventTag[]> {
+  public getTags(eventId: string): Observable<Tag[]> {
     return this.afs.collection<EventTag>('/event_tags', ref => ref.where('eventId', '==', eventId)).get().pipe(
       switchMap(eventTags => {
         let tagIds: string[] = [];
         eventTags.forEach(t => tagIds.push(t.data().tagId));
         if (tagIds.length > 0) {
-          return this.afs.collection<EventTag>('tag', ref => ref.where(firestore.FieldPath.documentId(), 'in', tagIds)).valueChanges();
+          return this.afs.collection<EventTag>('tag', ref => ref.where(firestore.FieldPath.documentId(), 'in', tagIds))
+            .valueChanges({ idField: 'id' });
         } else {
           return of(null);
         }
