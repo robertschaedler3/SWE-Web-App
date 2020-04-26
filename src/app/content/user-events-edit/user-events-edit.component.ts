4e2ef5d219ddac27b08c285d7083617a75eeb513
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-26 18:04:10 -0400

    display rsvp count

--- src/app/content/user-events-edit/user-events-edit.component.ts
+++ src/app/content/user-events-edit/user-events-edit.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { EventService } from '../../services/event.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 
 import { CreateEventComponent } from '../create-event/create-event.component';
 import { AuthService } from '../../services/auth.service';
+import { EventService } from '../../services/event.service';
 import { StevensEvent } from '../../models/event.model';
 import { Tag } from '../../models/tag.model';
 import { FormData } from '../../content/create-event/create-event.component';
@@ -20,6 +21,7 @@ import { firestore } from 'firebase';
 export class UserEventsEditComponent {
 
   eventId = '';
+  rsvps: number;
 
   event$: Observable<StevensEvent>;
   tags$: Observable<Tag[]>;
@@ -28,13 +30,29 @@ export class UserEventsEditComponent {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private auth: AuthService,
-    private eventSvc: EventService
+    private eventSvc: EventService,
+    private _snackBar: MatSnackBar
   ) {
     this.route.params.subscribe(params => {
       this.eventId = params.id;
       this.event$ = this.eventSvc.getEvent(this.eventId);
+      this.eventSvc.getRsvpCount(this.eventId).subscribe(val => this.rsvps = val);
     });
+  }
+
+  private openSnackBar(message: string, action: string = 'Dismiss', duration: number = 2000) {
+    return this._snackBar.open(message, action, {
+      duration: duration,
+    });
+  }
+
+  close(message?: string) {
+    if (message) {
+      this.openSnackBar(message);
+    }
+    this.router.navigate(['my-events']);
   }
 
   update() {
@@ -60,11 +78,25 @@ export class UserEventsEditComponent {
       }
 
       this.eventSvc.updateEvent(this.eventId, event, this.createComponent.tags);
+      this.close(`${title} successfully updated!`)
     });
   }
 
   delete() {
-    this.eventSvc.deleteEvent(this.eventId);
+    let snackBarRef = this.openSnackBar('Event deleted.', "Undo", 5000);
+    let undone = false;
+    snackBarRef.afterDismissed().subscribe(() => {
+      if (!undone) {
+        this.eventSvc.deleteEvent(this.eventId);
+      }
+    })
+
+    snackBarRef.onAction().subscribe(() => {
+      undone = true;
+      this.router.navigate(['my-events', this.eventId]);
+    })
+
+    this.close();
   }
 
 }
