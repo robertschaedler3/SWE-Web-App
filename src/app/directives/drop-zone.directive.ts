f48e76d9f432172064f03ce752ad3ea9f9b8abbc
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-26 18:11:35 -0400

    add drop zone directive

--- src/app/directives/drop-zone.directive.ts
+++ src/app/directives/drop-zone.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { Directive, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appDropZone]'
+  selector: '[dropZone]'
 })
 export class DropZoneDirective {
 
-  constructor() { }
+  @Output() dropped = new EventEmitter<FileList>();
+  @Output() hovered = new EventEmitter<boolean>();
+
+  @HostListener('drop', ['$event'])
+  onDrop($event) {
+    console.log('dropped')
+    $event.preventDefault();
+    this.dropped.emit($event.dataTransfer.files);
+    this.hovered.emit(false);
+  }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver($event) {
+    console.log('drag over')
+    $event.preventDefault();
+    this.hovered.emit(true);
+  }
+
+  @HostListener('dragleave', ['$event'])
+  onDragLeave($event) {
+    console.log('drag leave')
+    $event.preventDefault();
+    this.hovered.emit(false);
+  }
 
 }
