6a22a61dcbd6692616e8b59d1cead6fc9fc0c8b4
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-26 18:02:54 -0400

    update child route formatting

--- src/app/content/user-events/user-events.component.ts
+++ src/app/content/user-events/user-events.component.ts
@@ -3,32 +3,34 @@ import { Observable } from 'rxjs';
 import { StevensEvent } from '../../models/event.model';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { map } from 'rxjs/operators';
-import { listFader } from '../../animations/load-animation';
+import { listFader, fadeUp } from '../../animations/load-animation';
 
 @Component({
   selector: 'app-user-events',
   templateUrl: './user-events.component.html',
   styleUrls: ['./user-events.component.scss'],
-  animations: [listFader]
+  animations: [listFader, fadeUp]
 })
 export class UserEventsComponent implements OnInit {
 
   events$: Observable<StevensEvent[]>;
+  events: StevensEvent[] = [];
 
-  showOutlet: boolean;
+  showOutlet: boolean = false;
+  rsvps: number;
 
   constructor(
     private afs: AngularFirestore,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
   ) { }
 
   ngOnInit(): void {
-    this.afAuth.authState.pipe(
-      map(user => {
-        return this.afs.collection<StevensEvent>('/event', ref => ref.where('authorId', '==', user.uid)).valueChanges({ idField: 'id' });
-      })
-    ).subscribe(events => this.events$ = events);
+    this.afAuth.authState.subscribe(user => {
+      this.afs.collection<StevensEvent>('/event', ref => ref.where('authorId', '==', user.uid)).valueChanges({ idField: 'id' }).subscribe(events => {
+        this.events = events;
+        console.log(events)
+      });
+    });
   }
 
   onActivate(event: any) {
